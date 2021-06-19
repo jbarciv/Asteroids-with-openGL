bcca07bec69f3bf96daa8e411e298f7319efb526
Celia <ramoscelia3@gmail.com>
2021-06-19 16:45:37 +0200

    instructions

--- Asteroids/Angel.cpp
+++ Asteroids/Angel.cpp
@@ -26,9 +26,9 @@ Angel::Angel()
   tspeed[Y] = 0;
   tspeed[Z] = 0;
 
-  color[R] = 1; 
-  color[G] = 0.0;
-  color[B] = 0.0;
+  color[R] = 0.4; 
+  color[G] = 0.1;
+  color[B] = 0.3;
 
   status = INACTIVE;
 }
